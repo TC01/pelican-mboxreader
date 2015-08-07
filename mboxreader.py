b08e55c53a6cc4bc48f5a11e3f69390b9bbaa645
Your Name <rosser.bjr@gmail.com>
2015-08-07 00:21:16 -0400

    Added pelican 3.3 compatibility code (marked) for testing purposes. Fixed authorship, with a sledge hammer.

--- mboxreader.py
+++ mboxreader.py
@@ -1,18 +1,27 @@
 from pelican import signals
 from pelican.generators import ArticlesGenerator
-from pelican.contents import Article
+from pelican.contents import Article, Author
 from pelican.utils import process_translations
 
-from operator import attrgetter
+from operator import attrgetter, itemgetter
 from collections import defaultdict
 
 import mailbox
+import logging
+import random
 
 # Other dependency! dateutil.
 from dateutil import parser
 
+# Markdown-- a half-decent plaintext -> HTML converter, for now.
+try:
+    from markdown import Markdown
+except ImportError:
+    Markdown = False  # NOQA
+
 # For now hardcode this.
 mboxPath = '/home/bjr/Programming/python/pelican/security.mbox'
+categoryName = 'Security'
 
 class MboxGenerator(ArticlesGenerator):
 	
@@ -40,10 +49,14 @@ class MboxGenerator(ArticlesGenerator):
 		all_articles = []
 		subjects = []
 		for message in mbox.itervalues():
+			# Get author name.
 			author = message['from']
-
+			author = author[:author.find(' <')]
+			author = author.replace('"', '').replace("'", '')
+			
 			subject = message['subject']
 			# Hack to handle multiple subjects.
+			# This is bad because the subject is also the title; we should slugify the title first.
 			if subject in subjects:
 				subject += "%d"
 				count = 2
@@ -54,15 +67,14 @@ class MboxGenerator(ArticlesGenerator):
 				subject = testSubject
 			subjects.append(subject)
 
-			# This is a bad hack for mailman formatting, we should probably use mbox name by default.
-			# Or, make it configurable. Yay configuration.
-			category = subject[subject.find(']') + 2:]
 
 			date = parser.parse(message['date'])
-			content = message.get_payload()
+			content = Markdown().convert(message.get_payload())
 
-			metadata = {'title':subject, 'date':date, 'category':category}
+			metadata = {'title':subject, 'date':date, 'category':categoryName, 'authors':[author]}
 			article = Article(content=content, metadata=metadata, settings=self.settings, source_path=None, context=self)
+			# This seems like it cannot happen... but it does without fail. 3.3?
+			article.author = article.authors[0]
 			all_articles.append(article)
 
 		# Continue with the rest of ArticleGenerator.
@@ -73,6 +85,26 @@ class MboxGenerator(ArticlesGenerator):
 
 		# Disabled for 3.3 compatibility, great.
 		#signals.article_generator_pretaxonomy.send(self)
+
+		# Added for 3.3 compatibility.
+		# create tag cloud
+		tag_cloud = defaultdict(int)
+		for article in self.articles:
+			for tag in getattr(article, 'tags', []):
+				tag_cloud[tag] += 1
+
+		tag_cloud = sorted(tag_cloud.items(), key=itemgetter(1), reverse=True)
+		tag_cloud = tag_cloud[:self.settings.get('TAG_CLOUD_MAX_ITEMS')]
+
+		tags = list(map(itemgetter(1), tag_cloud))
+		if tags:
+			max_count = max(tags)
+		steps = self.settings.get('TAG_CLOUD_STEPS')
+
+		# calculate word sizes
+		self.tag_cloud = [(tag, int(math.floor(steps - (steps - 1) * math.log(count) / (math.log(max_count) or 1)))) for tag, count in tag_cloud]
+		# put words in chaos
+		random.shuffle(self.tag_cloud)
 
 		for article in self.articles:
 			# only main articles are listed in categories and tags
@@ -96,7 +128,7 @@ class MboxGenerator(ArticlesGenerator):
 		self.authors = list(self.authors.items())
 		self.authors.sort()
 
-		self._update_context(('articles', 'dates', 'tags', 'categories', 'authors', 'related_posts'))
+		self._update_context(('articles', 'dates', 'tags', 'categories', 'authors'))
 		# Disabled for 3.3 compatibility for now, great.
 		#self.save_cache()
 		#self.readers.save_cache()
@@ -109,3 +141,4 @@ def get_generators(pelican_object):
 
 def register():
 	signals.get_generators.connect(get_generators)
+	pass
